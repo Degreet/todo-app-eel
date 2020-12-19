1c7655f0131c14fd4ebc3660509a262503030a5e
Degreet <degreetpro@gmail.com>
2020-12-19 15:25:45 +0200

    mongodb connect

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import eel
+from config import host
+from pymongo import MongoClient
+
+cluster = MongoClient(host)
+db = cluster["todo-app-eel"]
+
+@eel.expose
+def reg_user():
+  pass
 
 eel.init("web")
 eel.start("index.html", size=(800, 600))
